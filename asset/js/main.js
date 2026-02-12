20f9a59da1a21f7d92a304d5f8754f5fc10c1938
yujin <syj20110@naver.com>
2026-02-12 13:11:35 +0900

    수정  작업 완료

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,12 +1,13 @@
 $(function () {
+
     gsap.registerPlugin(ScrollTrigger);
 
     /* 01. 헤더 색상 반전 (다크 모드 전환)*/
-    // 첫 번째 구간 (섹션 03 ~ 06)
+    // 첫 번째 구간
     ScrollTrigger.create({
-        trigger: "#section03",
+        trigger: ".sc-talent",
         start: "0% 10%",
-        endTrigger: "#section06",
+        endTrigger: "#section04",
         end: "0% 50%",
         toggleClass: {
             targets: "header, .header-logo, .path",
@@ -14,9 +15,9 @@ $(function () {
         },
     });
 
-    // 두 번째 구간 (섹션 12 ~ 마지막)
+    // 두 번째 구간
     ScrollTrigger.create({
-        trigger: "#section12",
+        trigger: "#darkTrigger",
         start: "0% 10%",
         endTrigger: "#endDark2",
         end: "0% 0%",
@@ -27,7 +28,7 @@ $(function () {
     });
 
 
-    /* 02. 인트로 영역 애니메이션 (섹션 01) */
+    /* 02. 인트로 영역 애니메이션 */
     const intro = gsap.timeline({
         scrollTrigger: {
             trigger: "#section01",
@@ -43,21 +44,26 @@ $(function () {
         },
     });
 
-    intro.to("video", { filter: "brightness(0.3)" }, 0); // 비디오 어둡게
-    intro.to(".step01", { opacity: 1 }, 0); // 텍스트 Step 01 등장
+    intro.to("video", { filter: "brightness(0.3)" }, 0);
+    // 비디오 어둡게
+    intro.to(".step01", { opacity: 1 }, 0);
+    // 텍스트 Step 01 등장
     intro.to(".step01", {
         opacity: 0,
         onStart() { $("header").addClass("active"); },
         onReverseComplete() { $("header").removeClass("active"); },
     });
-    intro.to(".step02", { opacity: 1 }); // 텍스트 Step 02
+    intro.to(".step02", { opacity: 1 });
+    // 텍스트 Step 02
     intro.to(".step02", { opacity: 0 });
-    intro.to(".step03", { opacity: 1 }); // 텍스트 Step 03
+    intro.to(".step03", { opacity: 1 });
+    // 텍스트 Step 03
     intro.to(".step03", { opacity: 0 });
-    intro.to(".step04", { opacity: 1 }); // 텍스트 Step 04
+    intro.to(".step04", { opacity: 1 });
+    // 텍스트 Step 04
 
 
-    /* 03. 포토 슬라이더 애니메이션 (섹션 02) */
+    /* 03. 포토 슬라이더 애니메이션 */
     const sect02 = gsap.timeline({
         scrollTrigger: {
             trigger: "#section02",
@@ -67,12 +73,17 @@ $(function () {
         },
     });
 
-    sect02.to("#section02 .img-wrap img", { filter: "brightness(0.5)" }, 0); // 이미지 어둡게
-    sect02.to("#section02 .text-wrap01", { opacity: 1 }, 0); // 텍스트 등장
-    sect02.to("#section02 .img-wrap img", { filter: "brightness(1)" }, 1); // 다시 밝게
-    sect02.to("#section02 .text-wrap01 .t01", { xPercent: 100 }, 1); // 좌우 텍스트 벌어짐
+    sect02.to("#section02 .img-wrap img", { filter: "brightness(0.5)" }, 0);
+    // 이미지 어둡게
+    sect02.to("#section02 .text-wrap01", { opacity: 1 }, 0);
+    // 텍스트 등장
+    sect02.to("#section02 .img-wrap img", { filter: "brightness(1)" }, 1);
+    // 다시 밝게
+    sect02.to("#section02 .text-wrap01 .t01", { xPercent: 100 }, 1);
+    // 좌우 텍스트 벌어짐
     sect02.to("#section02 .text-wrap01 .t03", { xPercent: -100 }, 1);
-    sect02.to("#section02 .text-wrap01", { opacity: 0 }, 1.5); // 텍스트 퇴장
+    sect02.to("#section02 .text-wrap01", { opacity: 0 }, 1.5);
+    // 텍스트 퇴장
 
     // 이미지 순차적으로 위로 걷힘 (Clip-Path)
     sect02.to("#section02 .img-wrap .img:nth-child(3)", { clipPath: "inset(0 0 100% 0)", ease: "none" }, 2);
@@ -81,34 +92,34 @@ $(function () {
     sect02.to("#section02 .text-wrap02", { opacity: 1 }, 3);
 
 
-    /* 04. 인트로덕션 영역 (섹션 04) */
-    const sect04 = gsap.timeline({
+    /* 04. visual */
+    const tlVisual = gsap.timeline({
         scrollTrigger: {
-            trigger: "#section04",
+            trigger: ".sc-talent .visual",
             start: "0% 60%",
             end: "100% 100%",
             scrub: true,
         },
     });
 
-    sect04.to(".introduction-area .before", { width: "30%" }, 0);
-    sect04.to(".introduction-area .title-01", { xPercent: -170 }, 0);
-    sect04.to(".introduction-area .after", { width: "30%" }, 0);
-    sect04.to(".introduction-area .title-03", { xPercent: 130 }, 0);
+    tlVisual.to(".sc-talent .visual .before", { width: "30%" }, 0);
+    tlVisual.to(".sc-talent .visual .caption01", { xPercent: -170 }, 0);
+    tlVisual.to(".sc-talent .visual .after", { width: "30%" }, 0);
+    tlVisual.to(".sc-talent .visual .caption03", { xPercent: 130 }, 0);
 
 
-    /* 05. 가로 슬라이드 및 바디 다크모드 (섹션 06) */
+    /* 05. 가로 슬라이드 및 바디 다크모드 */
     ScrollTrigger.create({
-        trigger: "#section06",
+        trigger: "#section04",
         start: "0% 50%",
-        endTrigger: "#section12",
+        endTrigger: "#darkTrigger",
         end: "0% 20%",
         toggleClass: { targets: "body", className: "dark" },
     });
 
-    const sect06 = gsap.timeline({
+    const sect04 = gsap.timeline({
         scrollTrigger: {
-            trigger: "#section06",
+            trigger: "#section04",
             start: "0% 0%",
             end: "100% 100%",
             scrub: true,
@@ -116,7 +127,7 @@ $(function () {
         },
     });
 
-    sect06.to(".identity-slide-cont", {
+    sect04.to(".identity-slide-cont", {
         x: () => {
             const slideContent = document.querySelector(".identity-slide-cont");
             return -(slideContent.offsetWidth - window.innerWidth);
@@ -136,14 +147,14 @@ $(function () {
     });
     slogan.from(".banner-slogan .pink, .banner-slogan .blue", { x: -300 }, 0);
     slogan.from(".banner-slogan .green", { x: 300 }, 0);
-    slogan.to(".banner-slogan .slogan-text", { opacity: 1 }, 1);
-    slogan.to(".banner-slogan .blur-bg", { opacity: 1 }, 1);
+    slogan.to(".slogan-title", { opacity: 1 }, 1);
+    slogan.to(".blur-overlay", { opacity: 1 }, 1);
 
 
-    /* 07. 카드 겹치기 및 자물쇠 애니메이션 (섹션 09) */
-    const sect09 = gsap.timeline({
+    /* 07. 카드 겹치기 및 자물쇠 애니메이션 (hybrid-step-01) */
+    const step01 = gsap.timeline({
         scrollTrigger: {
-            trigger: "#section09",
+            trigger: "#hybrid-step-01",
             start: "0% 0%",
             end: "100% 150%",
             scrub: true,
@@ -151,92 +162,133 @@ $(function () {
         },
     });
 
-    sect09.to("#section09 .slide-cont", .5, {
-        x: () => $("#section09 .slide-head").outerWidth() * -1,
+    step01.to("#hybrid-step-01 .slide-cont", .5, {
+        x: () => $("#hybrid-step-01 .slide-head").outerWidth() * -1,
     });
 
     // 카드 순차적 겹침
-    sect09.to("#section09 .slide-item:nth-of-type(2)", 1, { xPercent: -100, x: -40 }, 'a');
-    sect09.to("#section09 .slide-item:nth-of-type(3)", 1, { xPercent: -200, x: -80 }, 'a');
-    sect09.to("#section09 .slide-item:nth-of-type(4)", 1, { xPercent: -300, x: -120 }, 'a');
+    step01.to("#hybrid-step-01 .slide-item:nth-of-type(2)", 1, { xPercent: -100, x: -40 }, 'a');
+    step01.to("#hybrid-step-01 .slide-item:nth-of-type(3)", 1, { xPercent: -200, x: -80 }, 'a');
+    step01.to("#hybrid-step-01 .slide-item:nth-of-type(4)", 1, { xPercent: -300, x: -120 }, 'a');
 
     // 자물쇠 이미지 교체
-    sect09.to("#section09 .default", 0.5, { opacity: 0 }, 'b-=1');
-    sect09.to("#section09 .lock", 0.5, { opacity: 1 }, 'b-=0.5');
-    sect09.to("#section09 .lock", { opacity: 0 });
+    step01.to("#hybrid-step-01 .default", 0.5, { opacity: 0 }, 'b-=1');
+    step01.to("#hybrid-step-01 .lock", 0.5, { opacity: 1 }, 'b-=0.5');
+    step01.to("#hybrid-step-01 .lock", { opacity: 0 });
 
 
-    /* 08. 섹션 09 -> 10 전환 (카드 등장) */
+    /* 08. hybrid-step-01 -> 02 (카드 등장) */
     ScrollTrigger.create({
-        trigger: "#section09",
-        start: "bottom 150%",
+        trigger: "#hybrid-step-01",
+        start: "bottom 120%",
         onEnter: () => {
             const tl = gsap.timeline();
-            tl.set("#section09", { opacity: 0, pointerEvents: "none" });
-            tl.set("#section10 .left", { opacity: 1, pointerEvents: "auto" });
-            tl.set("#section10 .left .card-box", { opacity: 1 });
-            tl.set("#section10 .data-id_card", { opacity: 1 }, "+=0.1");
+            tl.set("#hybrid-step-01", {
+                opacity: 0,
+                pointerEvents: "none"
+            });
+            tl.set("#hybrid-step-02 .left", {
+                opacity: 1,
+                pointerEvents: "auto"
+            });
+            tl.set("#hybrid-step-02 .left .card-box", {
+                opacity: 1
+            });
+            tl.fromTo("#hybrid-step-02 .data-id_card",
+                { opacity: 0 },
+                {
+                    opacity: 1,
+                    ease: "power2.out"
+                },
+                "+=0.1"
+            );
         },
         onLeaveBack: () => {
-            gsap.set("#section09", { opacity: 1, pointerEvents: "auto" });
-            gsap.set("#section10 .left", { opacity: 0, pointerEvents: "none" });
-            gsap.set("#section10 .left .card-box", { opacity: 0 });
-            gsap.set("#section10 .data-id_card", { opacity: 0 });
+            gsap.set("#hybrid-step-01", { opacity: 1, pointerEvents: "auto" });
+            gsap.set("#hybrid-step-02 .left", { opacity: 0, pointerEvents: "none" });
+            gsap.set("#hybrid-step-02 .left .card-box", { opacity: 0 });
+
         },
         invalidateOnRefresh: true
     });
 
 
-    /* 09. 섹션 11 애니메이션 (카드 및 그라데이션) */
-    const sect11 = gsap.timeline({
-        scrollTrigger: {
-            trigger: "#section11",
-            start: "0% 0%",
-            end: "100% 100%",
-            scrub: true,
-            onEnter: () => {
-                gsap.set("#section11 .type-item-active, #section11 .cont", { opacity: 1 });
-                gsap.set("#section10", { opacity: 0, pointerEvents: "none" });
-            },
-            onLeaveBack: () => {
-                gsap.set("#section11 .type-item-active, #section11 .cont", { opacity: 0 });
-                gsap.set("#section10", { opacity: 1, pointerEvents: "auto" });
-            }
+    /* 09. hybrid-step-03 (카드 및 그라데이션) */
+    ScrollTrigger.create({
+        trigger: "#hybrid-step-03",
+        start: "top top",
+        invalidateOnRefresh: true,
+        onEnter: () => {
+            gsap.set("#hybrid-step-02", { opacity: 0, pointerEvents: "none" });
+            gsap.set("#hybrid-step-03 .type-item-active, #hybrid-step-03 .cont", { opacity: 1 });
         },
+        onLeaveBack: () => {
+            gsap.set("#hybrid-step-03 .type-item-active, #hybrid-step-03 .cont", { opacity: 0 });
+            gsap.set("#hybrid-step-02", { opacity: 1, pointerEvents: "auto" });
+        },
+        invalidateOnRefresh: true
     });
 
-    sect11.to("#section11 .slide-item:nth-of-type(2)", { xPercent: -100, x: -40 }, 'a+=0.1');
-    sect11.to("#section11 .slide-item:nth-of-type(3)", { xPercent: -200, x: -80 }, 'a+=0.1');
-    sect11.to("#section11 .slide-item:nth-of-type(4)", { xPercent: -300, x: -120 }, 'a+=0.1');
-    sect11.to("#section11 .data-user__text", { xPercent: -300 }, 'a+=0.1');
-    sect11.to("#section11 .data-user__text, #section11 .box-gradient", { opacity: 1, duration: 0.5 }, 'b');
-
-
-    /* 10. 인트로덕션 영역 02 (섹션 13) */
-    const sect13 = gsap.timeline({
+    const step03 = gsap.timeline({
         scrollTrigger: {
-            trigger: "#section13",
+            trigger: "#hybrid-step-03",
+            start: "top top",
+            end: "+=3000",
+            scrub: true
+        }
+    });
+
+    step03.to("#hybrid-step-03 .slide-item:nth-of-type(2)",
+        { xPercent: -100, x: -40 },
+        'a+=0.1'
+    );
+
+    step03.to("#hybrid-step-03 .slide-item:nth-of-type(3)",
+        { xPercent: -200, x: -80 },
+        'a+=0.1'
+    );
+
+    step03.to("#hybrid-step-03 .slide-item:nth-of-type(4)",
+        { xPercent: -300, x: -120 },
+        'a+=0.1'
+    );
+
+    step03.to("#hybrid-step-03 .data-user__text",
+        { xPercent: -300 },
+        'a+=0.1'
+    );
+
+    step03.to("#hybrid-step-03 .data-user__text, #hybrid-step-03 .box-gradient",
+        { opacity: 1 },
+        'b'
+    );
+
+
+    /* 10. 에셋 visual */
+    const acVisual = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".sc-asset .visual",
             start: "0% 60%",
             end: "100% 100%",
             scrub: true,
         },
     });
-    sect13.to(".introduction-area02 .before", { width: "30%" }, 0);
-    sect13.to(".introduction-area02 .title-01", { xPercent: -115 }, 0);
-    sect13.to(".introduction-area02 .after", { width: "30%" }, 0);
-    sect13.to(".introduction-area02 .title-03", { xPercent: 120 }, 0);
+    acVisual.to(".sc-asset .visual .before", { width: "30%" }, 0);
+    acVisual.to(".sc-asset .visual .caption01", { xPercent: -115 }, 0);
+    acVisual.to(".sc-asset .visual .after", { width: "30%" }, 0);
+    acVisual.to(".sc-asset .visual .caption03", { xPercent: 120 }, 0);
 
 
-    /* 11. 파이낸셜 가로 슬라이드 (섹션 14) */
-    const sect14 = gsap.timeline({
+    /* 11. 파이낸셜 가로 슬라이드 */
+    const finance = gsap.timeline({
         scrollTrigger: {
-            trigger: "#section14",
+            trigger: "#finance-slide",
             start: "0% 0%",
-            end: "100% 100%",
+            end: "+=200%",
             scrub: true,
-            toggleClass: { targets: "#section14 .slide-down", className: "active" },
+            toggleClass: { targets: "#finance-slide .slide-down", className: "active" },
             onUpdate: (self) => {
-                const slideDown = document.querySelector("#section14 .slide-down");
+                const slideDown = document.querySelector("#finance-slide .slide-down");
                 if (self.progress > 0.5) slideDown.classList.add("future");
                 else slideDown.classList.remove("future");
             },
@@ -244,45 +296,45 @@ $(function () {
         },
     });
 
-    sect14.to("#section14 .slide-cont", {
+    finance.to("#finance-slide .slide-cont", {
         x: () => {
-            const contentWidth = $("#section14 .slide-cont").outerWidth();
+            const contentWidth = $("#finance-slide .slide-cont").outerWidth();
             const viewportWidth = $(window).innerWidth();
             return (contentWidth - viewportWidth) * -1;
         },
     }, 0);
 
 
-    /* 12. 크리에이터 ID 인트로 (섹션 15) */
-    const sect15 = gsap.timeline({
+    /* 12. 앰배서더 ID 인트로 */
+    const ambassador = gsap.timeline({
         scrollTrigger: {
-            trigger: "#section15",
+            trigger: ".ambassador-intro",
             start: "0% 0%",
             end: "100% 100%",
             scrub: true,
         },
     });
-    sect15.to("#section15 .wrap, #section15 .arrow-down", { opacity: 1 }, 0);
-    sect15.to("#section15 .wrap, #section15 .arrow-down", { opacity: 0 }, 1);
+    ambassador.to(".ambassador-intro .creator-txt, .ambassador-intro .arrow-down", { opacity: 1 }, 0);
+    ambassador.to(".ambassador-intro .creator-txt, .ambassador-intro .arrow-down", { opacity: 0 }, 1);
 
 
-    /* 13. 가로 슬라이드 (섹션 16) */
-    const sect16 = gsap.timeline({
+    /* 13. 가로 슬라이드  */
+    const ambSlide = gsap.timeline({
         scrollTrigger: {
-            trigger: "#section16",
+            trigger: "#ambassador-slide",
             start: "0% 0%",
             end: "100% 90%",
             scrub: true,
             onEnter: () => {
-                document.querySelector("#section16 .slide-item:nth-child(1)").classList.add("is-active");
+                document.querySelector("#ambassador-slide .slide-item:nth-child(1)").classList.add("is-active");
             },
             invalidateOnRefresh: true,
         },
     });
 
-    sect16.to("#section16 .slide-cont", {
+    ambSlide.to("#ambassador-slide .slide-cont", {
         x: () => {
-            const contentWidth = $("#section16 .slide-cont").outerWidth();
+            const contentWidth = $("#ambassador-slide .slide-cont").outerWidth();
             const viewportWidth = $(window).innerWidth();
             return (contentWidth - viewportWidth) * -1;
         },
